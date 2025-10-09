c204534906767e25ae4e9b66ce1c069fbfb2feaf
potochon <gonzalomendoza1809@gmail.com>
2025-10-09 02:39:22 -0500

    apis mejoradas

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -36,7 +36,14 @@ func main() {
 	app := fiber.New()
 
 	// Configurar CORS
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000", // URL de tu frontend Next.js
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
+		AllowCredentials: true,
+		ExposeHeaders:    "Content-Length",
+		MaxAge:           86400, // 24 horas en segundos
+	}))
 
 	// Inicializar dependencias
 	habitacionRepo := repository.NewHabitacionRepository(db)
